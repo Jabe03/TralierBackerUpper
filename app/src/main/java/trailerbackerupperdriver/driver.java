43a34da606f5357c2c6b93e3c66f09b7cd5b8d24
jbergthold <92237063+Jabe03@users.noreply.github.com>
2023-10-03 15:26:54 -0500

    Initial code structure
    Structure for app, subject to change

---
+++ app/src/main/java/trailerbackerupperdriver/driver.java
@@ -0,0 +1,2 @@
+package trailerbackerupperdriver;
+//TODO: this is a placeholder for all of the trailerbackerupper code that will be written. All code that calculates values for the trailerbackerupper app goes here. Does not have to be in java I assume
